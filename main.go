278008ce9cd372006664326222b762db1d3a8bca
Giuseppe Capizzi <gcapizzi@pivotal.io>
2019-06-14 16:36:51 +0300

    Read test supports concurrent execution
    Co-authored-by: Danail Branekov <danailster@gmail.com>

--- main.go
+++ main.go
@@ -2,29 +2,33 @@ package main
 
 import (
 	"fmt"
-	"io"
 	"math/rand"
 	"os"
 	"strconv"
+	"sync"
 	"time"
 
 	uuid "github.com/nu7hatch/gouuid"
 )
 
 func main() {
-	operation := os.Args[1]
+	concurrentOps, err := strconv.Atoi(os.Args[1])
+	if err != nil {
+		panic(err)
+	}
+	operation := os.Args[2]
 
 	switch operation {
 	case "w":
-		size, err := strconv.Atoi(os.Args[2])
+		size, err := strconv.Atoi(os.Args[3])
 		if err != nil {
 			panic(err)
 		}
-		count, err := strconv.Atoi(os.Args[3])
+		count, err := strconv.Atoi(os.Args[4])
 		if err != nil {
 			panic(err)
 		}
-		avg, err := runWriteTest(size, count)
+		avg, err := runWriteTest(concurrentOps, size, count)
 		if err != nil {
 			panic(err)
 		}
@@ -35,8 +39,13 @@ func main() {
 	fmt.Println("DONE")
 }
 
-func runWriteTest(size, count int) (time.Duration, error) {
+func runWriteTest(concurrentOps, size, count int) (time.Duration, error) {
 	random := rand.New(rand.NewSource(42))
+	data := make([]byte, size)
+	_, err := random.Read(data)
+	if err != nil {
+		return 0, err
+	}
 
 	fmt.Printf("Running write test with file size %d and file count %d\n\n", size, count)
 	return average(func() (time.Duration, error) {
@@ -54,24 +63,66 @@ func runWriteTest(size, count int) (time.Duration, error) {
 		}()
 
 		startTime := time.Now()
-		_, err = io.CopyN(file, random, int64(size))
+		_, err = file.Write(data)
 		if err != nil {
 			return 0, err
 		}
 		return time.Since(startTime), nil
-	}, count)
+	}, concurrentOps, count)
 }
 
-func average(testFunc func() (time.Duration, error), count int) (time.Duration, error) {
-	var totalRuntime time.Duration
-	for i := 0; i < count; i++ {
-		runTime, err := testFunc()
-		if err != nil {
-			return 0, err
-		}
-		totalRuntime += runTime
-		fmt.Printf("Run %d: %s\n", i, runTime.String())
+func average(testFunc func() (time.Duration, error), concurrentOps, count int) (time.Duration, error) {
+	durationChan := make(chan time.Duration)
+	errChan := make(chan error)
+
+	runWg := sync.WaitGroup{}
+	runWg.Add(concurrentOps)
+
+	for i := 0; i < concurrentOps; i++ {
+		go func() {
+			defer runWg.Done()
+
+			for i := 0; i < count; i++ {
+				runTime, err := testFunc()
+				if err != nil {
+					errChan <- err
+					return
+				}
+
+				durationChan <- runTime
+				fmt.Printf("%s\n", runTime.String())
+			}
+		}()
 	}
 
-	return time.Duration(int64(totalRuntime) / int64(count)), nil
+	collectWg := sync.WaitGroup{}
+	collectWg.Add(1)
+	var totalRuntime time.Duration
+	var operationErr error
+	go func() {
+		defer collectWg.Done()
+
+		for {
+			select {
+			case d, ok := <-durationChan:
+				if !ok {
+					return
+				}
+				totalRuntime += d
+			case err, ok := <-errChan:
+				if !ok {
+					return
+				}
+				operationErr = err
+			}
+		}
+	}()
+
+	runWg.Wait()
+	close(durationChan)
+	close(errChan)
+
+	collectWg.Wait()
+
+	return time.Duration(int64(totalRuntime) / int64(count)), operationErr
 }
